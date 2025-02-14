98aaf1836059d57713798fe4fb914fcd38de9d1b
janjan <97529177+zxc8661@users.noreply.github.com>
2025-02-14 20:39:35 +0900

    9단계 완료

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,20 @@
 package org.example
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.io.File
+
 fun main() {
     println("명언앱 시작")
-    var lastId=0
+    var lastId= readTxtFile() ?:0
     val Datas = mutableListOf<Data>()
+
+
+    for(i in 1..lastId){
+        val data = readJsonFile(i)
+        if(data!=null) Datas.add(data)
+    }
 
 
     while(true){
@@ -20,7 +31,9 @@ fun main() {
             val author = readlnOrNull()!!.trim()
 
             val id=++lastId
-           Datas.add(Data(id,content,author))
+            val newData=Data(id,content,author)
+           Datas.add(newData)
+            makeFile(newData,lastId)
         }else if(input[0]== "목록") {
             println("----------------------")
             Datas.asReversed().forEach{println("${it.id} / ${it.author} / ${it.content}")}
@@ -57,11 +70,43 @@ fun main() {
                 author = newAuthor
             )
 
-
-
         }
     }
 }
 
+@Serializable
 data class Data ( val id: Int,val content: String,val  author:String)
 
+fun makeFile(data:Data, lastId:Int){
+    val directory = File("db/wiseSaying")
+    if(!directory.exists()) directory.mkdirs()
+
+    val jsonString = Json {prettyPrint=true}.encodeToString(data)
+    File("db/wiseSaying/${data.id}.json").writeText(jsonString)
+
+    File("db/wiseSaying/lastId.txt").writeText(lastId.toString());
+}
+
+fun readTxtFile():Int?{
+    val file = File("db/wiseSaying/lastId.txt")
+    if(!file.exists()){
+        println("파일이 존재하지 않습니다")
+        return null
+    }
+    return file.readText().toInt()
+}
+
+fun readJsonFile(id:Int):Data?{
+
+    val file = File("db/wiseSaying/${id}.json")
+    if(!file.exists()) return null
+
+    val jsonString = file.readText()
+
+    return try{
+        Json{ignoreUnknownKeys=true}.decodeFromString<Data>(Data.serializer(),jsonString)
+    }catch (e:Exception){
+        println("파싱오류")
+        null
+    }
+}
