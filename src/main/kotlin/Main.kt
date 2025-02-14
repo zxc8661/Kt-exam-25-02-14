d212f4944e1fc3308416434c1ffef1df37fa9782
janjan <97529177+zxc8661@users.noreply.github.com>
2025-02-14 15:00:58 +0900

    7단ãê ³ 오완료

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,13 +7,13 @@ fun main() {
 
 
     while(true){
-        print("명언) ")
-        val input = readlnOrNull()!!.trim()  //!!의 의미 : 해당 객체는 null 일리 없으니 null 처리 하지마
+        print("명령) ")
+        val input = readlnOrNull()!!.trim().split("?","=")  //!!의 의미 : 해당 객체는 null 일리 없으니 null 처리 하지마
 
 
-        if(input=="종료") {
+        if(input[0]=="종료") {
             break
-        }else if(input == "등록"){
+        }else if(input[0] == "등록"){
             print("명언 : ")
             val content = readlnOrNull()!!.trim()
             print("작가: ")
@@ -21,13 +21,22 @@ fun main() {
 
             val id=++lastId
            Datas.add(Data(id,content,author))
-        }else if(input== "목록"){
+        }else if(input[0]== "목록") {
             println("----------------------")
-            for(data in Datas){
-                println("${data.id} / ${data.author} / ${data.content}" )
+            for (data in Datas) {
+                println("${data.id} / ${data.author} / ${data.content}")
+            }
+        }else if(input[0] == "삭제"){
+           val result = Datas.removeAll{it.id==input[2].toInt()}
+
+            if(result) {
+                println("${input[2]}번 명언이 삭제되었습니다.")
+            }else{
+                println("${input[2]}번 명언이 존재하지 않습니다.")
             }
         }
     }
 }
 
 data class Data ( val id: Int,val content: String,val  author:String)
+
