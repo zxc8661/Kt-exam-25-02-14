6ba050e7c2f71c0e5c66337a136329495e9991ea
janjan <97529177+zxc8661@users.noreply.github.com>
2025-02-14 12:47:30 +0900

    2단계

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,13 @@ fun main() {
         val input = readlnOrNull()!!.trim()  //!!의 의미 : 해당 객체는 null 일리 없으니 null 처리 하지마
 
 
-        if(input=="종료")
+        if(input=="종료") {
             break
+        }else if(input == "등록"){
+            print("명언 : ")
+            val content = readlnOrNull()!!.trim()
+            print("작가: ")
+            val author = readlnOrNull()!!.trim()
+        }
     }
 }
